1b88c2704bc4864816d8c96ce9443b751f1a7fc3
LazyConi <wangkangn@hotmail.com>
2017-07-10 16:03:22 -0700

    Update testing in mappingProject.py

--- MappingProject.py
+++ MappingProject.py
@@ -41,12 +41,17 @@ class MappingProject:
                 originals += [k]
         return originals
         
-#mp = MappingProject()
-#
-#mp.readProjectKmlFiles("stitchTest/footprints.kml", "stitchTest/stitched footprint.kml", 2726, 2695)
-#print mp.originalCornerInStitch("DJI_0162.JPG")
-#print mp.fromOriginals(1000, 2000)
+mp = MappingProject()
 
+# My own test stitches
+#mp.readProjectKmlFiles("stitchTest/my-footprints.kml", "stitchTest/my-stitched-footprint.kml", 1000, 1000)
+#print mp.originalCornerInStitch("DJI_0162.JPG")
+#print mp.fromOriginals(499, 1000)
+#print mp.originalToStitch("DJI_0162.JPG", 2000, 0)
+#print mp.stitchToOriginal("DJI_0162.JPG", 0, 0)
+
+# Try mapping the point of interest from stitched map to individual originals
+#mp.readProjectKmlFiles("stitchTest/footprints.kml", "stitchTest/stitched footprint.kml", 2726, 2695)
 #print mp.stitchToOriginal("DJI_0162.JPG", 1363, 1342)
 #print mp.stitchToOriginal("DJI_0163.JPG", 1363, 1342)
 #print mp.stitchToOriginal("DJI_0169.JPG", 1363, 1342)
